3600ac0d0c25d0d454829896455769a4a6b2f60c
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-09-23 15:11:06 +0200

    Create functions to fetch products

---
+++ src/services/products/api.ts
@@ -0,0 +1,16 @@
+import axios, { type AxiosResponse } from 'axios'
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_PLATZI_API_URL,
+  withCredentials: false,
+  timeout: 60000
+})
+
+export default {
+  listProducts(): Promise<AxiosResponse> {
+    return client('/products')
+  },
+  getProduct(id: string | number): Promise<AxiosResponse> {
+    return client(`/products/${ id }`)
+  },
+}
