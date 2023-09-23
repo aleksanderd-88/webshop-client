05251af7f22bda24c5a1125dd4d2de5a61a4d2d4
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-09-23 20:48:22 +0200

    Implement card menu

--- src/components/molecules/Card/ProductCard.vue
+++ src/components/molecules/Card/ProductCard.vue
@@ -1,38 +1,56 @@
 <template>
-  <div class="product-card">
-    <img 
-      class="product-card__image" 
-      :src="get(product, 'category.image', '')" 
-      :alt="get(product, 'description', '')" 
-    />
+  <button 
+    type="button" 
+    class="product-card"
+    :class="modifiedClass"
+  >
+    <div 
+      class="product-card__content" 
+      @click="onCardClick()"
+    >
+      <img 
+        class="product-card__image" 
+        :src="get(product, 'category.image', '')" 
+        :alt="get(product, 'description', '')" 
+      />
 
-    <section class="product-card__specifications">
-      <h1 class="product-card__headline">Category</h1>
-      <h2 class="product-card__name">{{ get(product, 'category.name', '') }}</h2>
-      <p class="product-card__price">${{ get(product, 'price', '') }}</p>
-    </section>
+      <section class="product-card__specifications">
+        <h1 class="product-card__headline">Category</h1>
+        <h2 class="product-card__name">{{ get(product, 'category.name', '') }}</h2>
+        <p class="product-card__price">${{ get(product, 'price', '') }}</p>
+      </section>
 
-    <section class="product-card__actions">
-      <button type="button" class="product-card__action-btn">
-        <BaseIcon>favorite</BaseIcon>
+      <section class="product-card__actions">
+        <button type="button" class="product-card__action-btn">
+          <BaseIcon>favorite</BaseIcon>
+        </button>
+
+        <button type="button" class="product-card__action-btn">
+          <BaseIcon>compare_arrows</BaseIcon>
+        </button>
+
+        <button type="button" class="product-card__action-btn">
+          <BaseIcon>visibility</BaseIcon>
+        </button>
+      </section>
+    </div>
+
+    <div class="product-card__menu">
+      <button 
+        type="button" 
+        class="product-card__add-btn"
+      >
+        Add to cart
       </button>
-
-      <button type="button" class="product-card__action-btn">
-        <BaseIcon>compare_arrows</BaseIcon>
-      </button>
-
-      <button type="button" class="product-card__action-btn">
-        <BaseIcon>visibility</BaseIcon>
-      </button>
-    </section>
-  </div>
+    </div>
+  </button>
 </template>
 
 <script setup lang="ts">
 import BaseIcon from '../../atoms/Base/BaseIcon.vue';
 import get from 'lodash/get'
 import type { SingleProductType } from '@/stores/products/types';
-import { type PropType } from 'vue';
+import { type PropType, ref, computed } from 'vue';
 
 defineProps({
   product: {
@@ -41,19 +59,34 @@ defineProps({
     required: true
   }
 })
+
+const menuVisible = ref(false)
+
+const onCardClick = () => {
+  menuVisible.value = !menuVisible.value
+}
+
+const modifiedClass = computed(() => menuVisible.value && 'product-card--menu-visible')
 </script>
 
 <style lang="scss" scoped>
   .product-card {
-    border: 1px solid #eee;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 2rem;
+    $root: &;
+
     flex: 0 0 100%;
     max-width: 300px;
-    border-radius: 3px;
+    position: relative;
+    overflow-y: hidden;
+
+    &__content {
+      border: 1px solid #eee;
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 2rem;
+      border-radius: 3px;
+    }
 
     &__image {
       width: 100%;
@@ -105,6 +138,48 @@ defineProps({
       @media (min-width: 1024px) {
         &:hover {
           background-color: rgba(#999, .1);
+        }
+      }
+    }
+
+    &__menu {
+      background-color: #1e1f29;
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      left: 50%;
+      width: 95%;
+      opacity: 0;
+      cursor: default;
+      padding: 1rem;
+      border-radius: 3px 3px 0 0;
+      visibility: hidden;
+      transform: translate(-50%, 100%);
+      transition: transform .2s, opacity .2s, visibility .2s ease;
+    }
+
+    &__add-btn {
+      margin: auto;
+      background-color: #eee;
+      border-radius: 50px;
+      font-weight: 500;
+      visibility: hidden;
+      opacity: 0;
+      padding: 1rem 2rem;
+      transition: visibility .2s, opacity .2s ease;  
+    }
+
+    @media (max-width: 1024px) {
+      &--menu-visible {
+        #{$root}__menu {
+          opacity: 1;
+          visibility: visible;
+          transform: translate(-50%, 0);
+        }
+        
+        #{$root}__add-btn {
+          opacity: 1;
+          visibility: visible;
         }
       }
     }
